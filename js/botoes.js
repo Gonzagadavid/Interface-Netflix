260544ccdb31a7979eabe9e1d245070df1d019cd
Gonzagadavid <73129958+Gonzagadavid@users.noreply.github.com>
2020-10-27 14:54:57 -0300

    commit inicial

---
+++ js/botoes.js
@@ -0,0 +1,12 @@
+
+function botao1(){
+    window.location.href="https://www.youtube.com/watch?v=liV_LoMSteo";
+}
+function botao2(){
+    window.location.href="https://pt.wikipedia.org/wiki/Walter_O%27Brien";
+
+function mini1(){
+    window.location.hostname="https://www.youtube.com/watch?v=LwCL_LFtrgc";
+}
+
+}
